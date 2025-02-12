fafe2ea03077604d5e0a231bb9b167a3830ccf65
ythollet <159007949+ythollet@users.noreply.github.com>
2025-02-12 14:47:17 +0100

    Update Manager.java

--- src/JavaProject/Manager.java
+++ src/JavaProject/Manager.java
@@ -61,25 +61,106 @@ public class Manager extends Utilisateur {
 	
 	
 	
+	// ---------------------- METHODE AJOUTER UTILISATEUR -----------------------//
+	public static void ajouter_utilisateur(String nom, String prenom, String poste, int jours_conge_restants, String mdp, String statut) {
+		String ligne;
+		int nb_lignes = 0;
+		String path_csv = "Ressources/Utilisateurs.csv";
+		
+		// Compter le nombre de lignes
+	    try (BufferedReader br = new BufferedReader(new FileReader(path_csv))) {
+	        // Lire et ignorer la première ligne (en-tête)
+	        br.readLine();
+	       
+	        // Lire chaque ligne du fichier
+	        while ((ligne = br.readLine()) != null) {
+	        	nb_lignes++;
+	            }
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	    
+	    // Ajouter le nouvel utilisateur
+        try (FileWriter writer = new FileWriter(path_csv, true);
+                BufferedWriter bw = new BufferedWriter(writer);
+                PrintWriter out = new PrintWriter(bw)) { // PrintWriter facilite l'écriture des lignes
+
+               // Construire la ligne utilisateur
+               String nouvelleLigne = (nb_lignes + 1) + ";" +
+                                       nom + ";" +
+                                       prenom + ";" +
+                                       poste + ";" +
+                                       jours_conge_restants + ";" +
+                                       mdp + ";" +
+                                       statut;
+
+               out.println(nouvelleLigne); //  Ajout direct avec un saut de ligne
+
+               System.out.println("Utilisateur ajouté avec succès !");
+           } catch (IOException e) {
+               e.printStackTrace();
+           }
+       }
+	
+	
+	
+	
+	
+	
 	// ---------------------- METHODE SUPPRIMER UTILISATEUR -----------------------//
 	public static void supprimer_utilisateur(String nom, String prenom) {
 		String ligne;
+		String path_csv = "Ressources/Utilisateurs.csv";
+		File fichier_original = new File(path_csv);
+        File new_file = new File("Ressources/new_utilisateurs.csv"); // Fichier temporaire
+
+
+		boolean utilisateur_trouve = false;
+
 		
-	    // Ajouter le nouvel utilisateur
-       try (FileWriter writer = new FileWriter(path_csv, true);
-               BufferedWriter bw = new BufferedWriter(writer);
-               PrintWriter out = new PrintWriter(bw)) { // PrintWriter facilite l'écriture des lignes
-       	// Chercher la ligne à supprimmer
-       	while(ligne = br.readLine() != null) {
-       		!! trouver le moyen d'identifier la ligne a supprimer
-       	}
-       	
-       		
-              System.out.println("Utilisateur ajouté avec succès !");
-          } catch (IOException e) {
-              e.printStackTrace();
-          }
+	    // On lit le fichier en mode ecriture
+        try (BufferedReader br = new BufferedReader(new FileReader(path_csv));
+             PrintWriter pw = new PrintWriter(new FileWriter(new_file))) { // PrintWriter facilite l'écriture des lignes
+
+        	// On parcours chaque ligne du fichier
+        	while((ligne = br.readLine()) != null) {
+        		// On sépare chaque ligne par les différentes colonnes
+        		String[] colonne = ligne.split(";");
+                String nom_csv = colonne[1];
+                String prenom_csv = colonne[2];
+                // Si l'utilisateur que l'on veut supprimer est trouvé, alors on ne le réécrit pas 
+                if (nom.equals(nom_csv) && prenom.equals(prenom_csv)) {
+                	System.out.println("On a trouvé l'utilisateur à supprimer");
+                	utilisateur_trouve = true;
+                	continue;
+                }
+                pw.println(ligne);
+
+        	}
+        	
+        		
+
+           } catch (IOException e) {
+               e.printStackTrace();
+           }
+        
+        
+        // On remplace le fichier d'origine par le fichier modifié
+        if (utilisateur_trouve) {
+        	try {
+                Files.delete(fichier_original.toPath()); // Supprime l'ancien fichier
+                Files.move(new_file.toPath(), fichier_original.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        	}
+        	catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            System.out.println("Utilisateur non trouvé, aucune suppression effectuée.");
+            new_file.delete(); // Supprime le fichier temporaire 
+        
 	}
+}
 }
 
 

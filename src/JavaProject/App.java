bdf864d952a50572cf5515b146ecf3db255d8203
ythollet <159007949+ythollet@users.noreply.github.com>
2025-02-13 15:16:38 +0100

    Add files via upload

--- src/JavaProject/App.java
+++ src/JavaProject/App.java
@@ -1,70 +1,20 @@
-package JavaProject;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.SwingUtilities;
-
-
 public class App {
 	
 	
-	public static List<Utilisateur> func_recup_data(String chemin_fichier) {
-		
-    
-    String ligne;
-    
-    // Creation des listes d'utilisateurs
-    List<Utilisateur> liste_utilisateur = new ArrayList<>();
-
-
-    try (BufferedReader br = new BufferedReader(new FileReader(chemin_fichier))) {
-        // Lire et ignorer la première ligne (en-tête)
-        br.readLine();
-       
-        // Lire chaque ligne du fichier
-        while ((ligne = br.readLine()) != null) {
-            // Récupérer les données du csv
-            String[] colonne = ligne.split(";");
-            String id_csv_str = colonne[0];
-            int id_csv = Integer.parseInt(id_csv_str);
-            String nom_csv = colonne[1];
-            String prenom_csv = colonne[2];
-            String poste_csv = colonne[3];
-            String jours_conges_restants_csv_str = colonne[4];
-            int jours_conges_restants_csv = Integer.parseInt(jours_conges_restants_csv_str);
-            String mdp_csv = colonne[5];
-            String statut_csv = colonne[6];
-            
-            // Création des nouveaux objets
-            liste_utilisateur.add(new Utilisateur(id_csv,nom_csv, prenom_csv, poste_csv, jours_conges_restants_csv, mdp_csv,statut_csv));
-                     
-            
-            }
-            System.out.println();
-        
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-    
-    return liste_utilisateur;
-}
 
 
 	
 	   public static void main(String[] args) {
 		   
-//		   Ouvre la fenêtre de connexion 
-		   SwingUtilities.invokeLater(() -> {
-	            LoginFrame frame = new LoginFrame();
-	            frame.setVisible(true);
-	        });
 		   
 		    // ETAPE 1 : On recupere les données du csv
-		    List<Utilisateur> liste_utilisateur = func_recup_data("resources/Utilisateurs.csv");
+		    List<Utilisateur> liste_utilisateur = Utilisateur.func_recup_data("Ressources\\Utilisateurs.csv");
 		   
 		   
 		    
@@ -93,7 +43,7 @@ public class App {
 	        	String utilisateur_mdp = utilisateur.mdp;
 	        	String utilisateur_statut = utilisateur.statut;
 	        
-	          // Si l'utilisateur est un manager	
+//	          // Si l'utilisateur est un manager	
 		      if(utilisateur_statut.equals("manager")) {
 		      	new_manager = new Manager(utilisateur_id, utilisateur_nom, utilisateur_prenom, utilisateur_poste, utilisateur_jours_conge_restants, utilisateur_mdp, utilisateur_statut);
 		      	liste_managers.add(new_manager);
@@ -105,22 +55,18 @@ public class App {
 		          	liste_employe.add(new_employe); 
 		      }
 	   }
-	      // Tests
 	      for (Employe employe:liste_employe) {
 	    	  System.out.println(employe.nom);
 	    	  System.out.println(employe.statut);
 	      }
-	      for (Manager manager:liste_managers) {
-	    	  System.out.println(manager.nom);
-	    	  System.out.println(manager.statut);
-	      }
-//	      Manager.ajouter_utilisateur("DUPONT", "Jean", "data analyst", 50, "motdepassedefou", "employe");
-
-	      Manager.supprimer_utilisateur("DUPONT", "Jean");
 	      
-		   	// ETAPE 3 : Ajouter un utilisateurON TESTE LES METHODES
+	      
+	      
+//		   	// ETAPE 3 :  ON TESTE LES METHODES
+////	      Manager.ajouter_utilisateur("DUPONT", "Jean", "data analyst", 50, "motdepassedefou", "employe");
+//	      Manager.supprimer_utilisateur("DUPONT", "Jean");
    }
 	
 
-
 }
+
--- src/JavaProject/Manager.java
+++ src/JavaProject/Manager.java
@@ -1,5 +1,3 @@
-package JavaProject;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -9,6 +7,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+
 public class Manager extends Utilisateur {
 	
 	// ---------------------- CONSTRUCTEURS -----------------------//
@@ -22,11 +21,14 @@ public class Manager extends Utilisateur {
 		super();
 	}
 	
+	
+	
+	
 	// ---------------------- METHODE AJOUTER UTILISATEUR -----------------------//
 	public static void ajouter_utilisateur(String nom, String prenom, String poste, int jours_conge_restants, String mdp, String statut) {
 		String ligne;
 		int nb_lignes = 0;
-		String path_csv = "resources/Utilisateurs.csv";
+		String path_csv = "Ressources/Utilisateurs.csv";
 		
 		// Compter le nombre de lignes
 	    try (BufferedReader br = new BufferedReader(new FileReader(path_csv))) {
@@ -71,9 +73,9 @@ public class Manager extends Utilisateur {
 	// ---------------------- METHODE SUPPRIMER UTILISATEUR -----------------------//
 	public static void supprimer_utilisateur(String nom, String prenom) {
 		String ligne;
-		String path_csv = "resources/Utilisateurs.csv";
+		String path_csv = "Ressources/Utilisateurs.csv";
 		File fichier_original = new File(path_csv);
-        File new_file = new File("resources/new_utilisateurs.csv"); // Fichier temporaire
+        File new_file = new File("temp_utilisateur"); // Fichier temporaire
 
 
 		boolean utilisateur_trouve = false;
@@ -122,7 +124,50 @@ public class Manager extends Utilisateur {
         
 	}
 }
+	
+	
+	// ---------------------- METHODE CALCULER SALAIRE -----------------------//
+	public static float calculer_salaire(String nom, String prenom) {
+		String path_th = "Ressources/taux_horraire_poste.csv";
+		String path_utilisateurs = "Ressources/Utilisateurs.csv";
+		String ligne;
+		boolean utilisateur_trouve = false;
+		// On lit le fichier utilisateur
+		 try (BufferedReader br_utilisateurs = new BufferedReader(new FileReader(path_utilisateurs))) {
+				  while((ligne = br_utilisateurs.readLine()) != null) {
+					  // On sépare la ligne en fonction des colonnes
+					  String[] colonne_utilisateur = ligne.split(";");
+					  String nom_utilisateur = colonne_utilisateur[1];
+					  String prenom_utilisateur = colonne_utilisateur[2];
+					  String poste_utilisateur =  colonne_utilisateur[3];
+					  // Si la personne pour qui on veut calculer le salaire est trouvée, on récupere le nom de son poste
+					  if (nom_utilisateur.equals(nom) && prenom_utilisateur.equals(prenom)) {
+						  // Si l'employé existe dans la base de données, on cherche son poste (qui nous donnera son salaire a l'heure)
+						  try (BufferedReader br_th = new BufferedReader(new FileReader(path_th))){
+							  while ((ligne = br_th.readLine()) != null){
+								  String[] colonne_th = ligne.split(";");
+								  String poste_th = colonne_th[0];
+								  float taux_horraire = Float.parseFloat(colonne_th[1]) ;
+								  // Si le poste de l'utilisateur est dans le fichier taux_horraire.csv, on récupere le taux horraire correspondant
+								  if (poste_utilisateur.equals(poste_th)) {
+									  return taux_horraire;
+								  }
+							  }
+						  }
+						  break;
+					  }
+				  }
+
+
+				  
+			  }
+     	catch (IOException e) {
+            e.printStackTrace();
+           
+        }
+		 return -1;
+		
+	}
+
 }
-
-
 
